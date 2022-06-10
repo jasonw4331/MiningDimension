3f70e3422d1133bb4570a2947c5e81f81f982add
jasonwynn10 <jasonwynn10@me.com>
2022-06-10 04:33:40 -0400

    Fixed shapeless recipe error

--- src/recipe/DurabilityShapelessRecipe.php
+++ src/recipe/DurabilityShapelessRecipe.php
@@ -5,42 +5,8 @@ namespace jasonwynn10\MiningDimension\recipe;
 use pocketmine\crafting\CraftingGrid;
 use pocketmine\crafting\ShapelessRecipe;
 use pocketmine\item\Durable;
-use pocketmine\item\Item;
-use pocketmine\utils\Utils;
 
 final class DurabilityShapelessRecipe extends ShapelessRecipe{
-	/** @var Item[] */
-	private $ingredients = [];
-	/** @var Item[] */
-	private $results;
-
-	/**
-	 * @param Item[] $ingredients No more than 9 total. This applies to sum of item stack counts, not count of array.
-	 * @param Item[] $results List of result items created by this recipe. If an item has a durability, the durability
-	 * of that item will be subtracted from the equivalent item in the crafting grid.
-	 * @noinspection PhpMissingParentConstructorInspection
-	 */
-	public function __construct(array $ingredients, array $results){
-		foreach($ingredients as $item){
-			//Ensure they get split up properly
-			if(count($this->ingredients) + $item->getCount() > 9){
-				throw new \InvalidArgumentException("Shapeless recipes cannot have more than 9 ingredients");
-			}
-
-			while($item->getCount() > 0){
-				$this->ingredients[] = $item->pop();
-			}
-		}
-
-		$this->results = Utils::cloneObjectArray($results);
-	}
-
-	/**
-	 * @return Item[]
-	 */
-	public function getResults() : array{
-		return Utils::cloneObjectArray($this->results);
-	}
 
 	public function getResultsFor(CraftingGrid $grid) : array{
 		$results = $this->getResults();
@@ -57,16 +23,9 @@ final class DurabilityShapelessRecipe extends ShapelessRecipe{
 		return $results;
 	}
 
-	/**
-	 * @return Item[]
-	 */
-	public function getIngredientList() : array{
-		return Utils::cloneObjectArray($this->ingredients);
-	}
-
 	public function getIngredientCount() : int{
 		$count = 0;
-		foreach($this->ingredients as $ingredient){
+		foreach($this->getIngredientList() as $ingredient){
 			$count += $ingredient->getCount();
 		}
 
@@ -77,7 +36,7 @@ final class DurabilityShapelessRecipe extends ShapelessRecipe{
 		//don't pack the ingredients - shapeless recipes require that each ingredient be in a separate slot
 		$input = $grid->getContents();
 
-		foreach($this->ingredients as $needItem){
+		foreach($this->getIngredientList() as $needItem){
 			if($needItem instanceof Durable) {
 				foreach($input as $j => $haveItem){
 					if($haveItem->equals($needItem, false, $needItem->hasNamedTag()) && $haveItem->getCount() - $needItem->getCount() >= 0) {
